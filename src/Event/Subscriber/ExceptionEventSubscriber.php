aeb14c0610314f7390e4df94a48c52fd22ad7008
Lukáš Bečvář <lordbecvold@gmail.com>
2024-10-08 12:51:51 +0200

    [+] exclude error 400 from exception log

--- src/Event/Subscriber/ExceptionEventSubscriber.php
+++ src/Event/Subscriber/ExceptionEventSubscriber.php
@@ -3,8 +3,10 @@
 namespace App\Event\Subscriber;
 
 use Psr\Log\LoggerInterface;
+use App\Exception\AppErrorException;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -45,14 +47,32 @@ class ExceptionEventSubscriber implements EventSubscriberInterface
      */
     public function onKernelException(ExceptionEvent $event): void
     {
-        // get the error caller
-        $errorCaler = $event->getThrowable()->getTrace()[0]['function'];
+        // get the exception object
+        $exception = $event->getThrowable();
 
-        // get the error message
-        $message = $event->getThrowable()->getMessage();
+        // get the error caller
+        $errorCaller = $exception->getTrace()[0]['function'];
 
         // check if the error caller is the logger
-        if ($errorCaler != 'handleError') {
+        if ($errorCaller != 'handleError') {
+            return;
+        }
+
+        // get the error message
+        $message = $exception->getMessage();
+
+        // define default exception code
+        $statusCode = 500;
+
+        // check if the object is valid exception
+        if ($exception instanceof HttpException) {
+            // get exception status code
+            $statusCode = $exception->getStatusCode();
+        }
+
+        // check if exception code is in the excluded codes
+        $excludedCodes = [400, 404, 426, 429, 501, 503];
+        if (in_array($statusCode, $excludedCodes)) {
             return;
         }
 
